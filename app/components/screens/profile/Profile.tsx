8e2743e29a7b35a8c3022ade1782d6655f412a20
Artem-Ky <artoad505@mail.ru>
2024-10-12 11:49:07 +0300

    feat: profile screen

--- app/components/screens/profile/Profile.tsx
+++ app/components/screens/profile/Profile.tsx
@@ -1,11 +1,15 @@
+import Button from '@/components/ui/Button/Button'
 import Layout from '@/components/ui/layout/Layout'
+import { useAuth } from '@/hooks/useAuth'
 import { FC } from 'react'
 import { View, Text } from 'react-native'
 
 export const Profile: FC = () => {
+	const {setUser} = useAuth();
+
 	return (
-		<Layout>
-			<Text>Profile</Text>
+		<Layout title='Профиль'>
+			<Button onPress={() => setUser(null)}>Выйти из аккаунта</Button>
 		</Layout>
 	)
 }
